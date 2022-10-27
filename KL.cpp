71942c8f999b86a5c052283aad476b6e23015e76
Damir10sorokin20 <114170327+Damir10sorokin20@users.noreply.github.com>
2022-10-27 18:19:51 +0300

    Add files via upload

---
+++ KL.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+int main() {
+//1
+	//int n;
+	//cin >> n;
+		//for (int i = 1; i <= n; i += 1) {
+			//cout << '&';
+	//}
+
+//while 1
+	//int a, b, i;
+	//cin >> a >> b;
+	//int i = b;
+	//while (i >= a) {
+
+		//cout << i << ' ';
+		//i -= 1;
+	//}
+	
+	//2
+	
+	//for (int i = 20; i <= 30; i += 2) {
+		//cout << i << ' ';
+	//}
+	//while 2
+	//int a, b;
+	//cin >> a>> b;
+	//int i = a;
+	//int s = 0;
+	//while (i <= b) {
+		//if (i % 2 == 0) {
+			//s = s + i;
+
+	//}
+		//i++;
+	//}
+	//cout << s << ' ';
+
+	//
+	//for3
+	//int n;
+	//int pr = 1;
+	//cin >> n;
+	//for (int i = 1; i <= n; i++) {
+		//if (i % 5 != 0) {
+			//pr = pr * i;
+		//}
+		
+	//}
+	//cout << pr << ' ';
+	
+	//while 3
+	//int n;
+	//cin >> n;
+	//double s = 0;
+	//double i = 1;
+	//while (i <= n) {
+		//s = s + 1 / i;
+		//i++;
+	//}
+	//cout << s << ' ';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}
